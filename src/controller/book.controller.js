d1b16b690a1fd06cec21575712945d4da328cbb4
edward <edwardlfish@gmail.com>
2025-12-17 20:11:58 +0200

    Refactor `addBook` controller to include transaction safety and deduplicate author records.

--- src/controller/book.controller.js
+++ src/controller/book.controller.js
@@ -2,7 +2,7 @@ import {Author, Book, Publisher} from "../model/index.js";
 import {sequelize} from "../config/database.js";
 
 export const addBook = async (req, res) => {
-    const t = await sequelize.transaction();
+    const t = await sequelize.transaction({readOnly: true});
     try {
         const {isbn, title, authors, publisher} = req.body;
         const existingBook = await Book.findByPk(isbn);
@@ -13,30 +13,31 @@ export const addBook = async (req, res) => {
             });
         }
         // Create or find the publisher
-        let publisherRecord = await Publisher.findByPk(publisher);
-        if (!publisherRecord) {
-            publisherRecord = await Publisher.create({publisher_name: publisher}, {transaction: t});
+        if (!await Publisher.findByPk(publisher, {transaction: t})) {
+            await Publisher.create({publisher_name: publisher}, {transaction: t});
         }
         // Process the authors
         const authorRecords = [];
         for (const author of authors) {
-            let authorRecord = await Author.findByPk(author.name);
+            let authorRecord = await Author.findByPk(author.name, {transaction: t});
             if (!authorRecord) {
                 authorRecord = await Author.create({
                     name: author.name,
                     birth_date: new Date(author.birthDate)
                 }, {transaction: t});
             }
-            authorRecords.push(authorRecord);
+            if (authorRecords.findIndex(a => a.name === authorRecord.name) === -1) {
+                authorRecords.push(authorRecord);
+            }
         }
         // Create a new book
         const book = await Book.create({isbn, title, publisher}, {transaction: t});
         await book.setAuthors(authorRecords, {transaction: t});
         await t.commit();
-        return res.status(201);
+        return res.status(201).send();
     } catch (e) {
         await t.rollback();
-        console.error('Error adding book:',e);
+        console.error('Error adding book:', e);
         return res.status(500).send({
             error: e.message,
             message: 'Failed to add book'
