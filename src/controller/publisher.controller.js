a9ae6ce7179c272e5d62bdec15016c262bfa7fef
edward <edwardlfish@gmail.com>
2025-12-21 20:51:56 +0200

    Optimize `findPublishersByAuthor` controller by replacing redundant book mapping with direct `DISTINCT` publisher query.

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -3,18 +3,16 @@ import {Author, Book} from "../model/index.js";
 export const findPublishersByAuthor = async (req, res) => {
     const author = await Author.findByPk(req.params.name);
     if (!author) {
-        return res.status(404).json({ error: 'Author not found' });
+        return res.status(404).json({error: 'Author not found'});
     }
-    const books = await Book.findAll({
+    const publishers = await Book.aggregate('publisher', 'DISTINCT', {
+        plain: false,
         include: {
-            model: Author, as: 'authors',
-            where: {name: req.params.name},
+            model: Author,
+            as: 'authors',
+            where: {name: author.name},
             through: {attributes: []}
-        },
-        attributes: ['publisher'],
-        raw: true,
-        group: ['publisher']
-    });
-    const publishers = books.map(book => book.publisher);
-    return res.json(publishers);
+        }
+    })
+    return res.json(publishers.map(p => p.DISTINCT));
 }
